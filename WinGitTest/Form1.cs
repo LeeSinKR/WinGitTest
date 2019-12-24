0d86128469da41be69c6c0cab64ea1f52f45d8eb
서선도 <tjtjseh1234@naver.com>
2019-12-24 16:15:33 +0900

    초기 커밋.

---
+++ WinGitTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinGitTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "버튼클릭";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Text = "버튼23";
+        }
+    }
+}
